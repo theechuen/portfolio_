3adbd33c15159094aac9ec3e121dd6fcb46a0cf4
Simon <simongo1121@gmail.com>
2023-12-09 19:00:23 +0800

    update components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import dynamic from 'next/dynamic';
+import React from 'react';
+import AboutMe from '../components/AboutMe';
+import Project from '../components/Project';
+import EmailSection from '../components/EmailSection';
+import Footer from '../components/Footer';
+import AchievementsSection from '../components/AchievementsSection';
 
-export default function Home(){
-  return(
-    <div>h1</div>
+//Server: not found error handler
+const HeroSection = dynamic(
+  () => import('../components/HeroSection'),
+  { ssr: false }
+);
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col ">
+        <main>
+        <HeroSection />
+        <AchievementsSection />
+        <AboutMe />
+        <Project />
+        <EmailSection />
+        </main>
+        <Footer />
+    </div>
   );
 }
